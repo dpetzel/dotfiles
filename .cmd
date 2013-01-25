0cd834105b9faeb681a6701e2540445c5f3d6220
Dave Petzel <davidpetzel@gmail.com>
2013-01-24 21:19:26 -0500

    a start

---
+++ .cmd
@@ -0,0 +1,17 @@
+:: This file is tended to be added to HKEY_CURRENT_USER\Software\Microsoft\Command Processor\AutoRun
+:: So that a standardized command prompt configuration can happen
+	@ECHO OFF
+	echo %date% %time%
+:: Setup some aliases
+
+	:: ls just feels more natural doesn't it?
+	DOSKEY ls=dir
+	
+	:: When using vagrant with bundler, I hate all the typing
+	DOSKEY bv=bundle exec vaggrant
+	
+:: Customize our prompt (give a linux feel)
+	:: Start with the time, dropping miliseconds ($T$H$H$H)
+	PROMPT=[%username%@%computername%]$$
+
+@ECHO ON
